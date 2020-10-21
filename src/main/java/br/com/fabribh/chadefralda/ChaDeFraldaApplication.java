e0fd7f34e8c95e016543b6bd58a04784249c2366
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-10-21 20:49:34 -0300

    Primeiro commit

---
+++ src/main/java/br/com/fabribh/chadefralda/ChaDeFraldaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabribh.chadefralda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaDeFraldaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaDeFraldaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fabribh/chadefralda/ChaDeFraldaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabribh.chadefralda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaDeFraldaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
