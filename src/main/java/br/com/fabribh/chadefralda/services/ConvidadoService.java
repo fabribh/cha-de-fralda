02e2a5c15fed234ca67d6441af91cbf8c4866b63
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2021-01-30 15:46:57 -0300

    Configurando o sorteio

--- src/main/java/br/com/fabribh/chadefralda/services/ConvidadoService.java
+++ src/main/java/br/com/fabribh/chadefralda/services/ConvidadoService.java
@@ -6,6 +6,7 @@ import br.com.fabribh.chadefralda.model.entities.Estoque;
 import br.com.fabribh.chadefralda.model.entities.Presente;
 import br.com.fabribh.chadefralda.model.modelExceptions.ChaException;
 import br.com.fabribh.chadefralda.model.repositories.ConvidadoRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +28,9 @@ public class ConvidadoService {
 
     @Autowired
     private PresenteService presenteService;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Transactional
     public ConvidadoDTO save(ConvidadoDTO dto) throws Exception {
@@ -66,8 +70,8 @@ public class ConvidadoService {
         ConvidadoDTO convidadoSorteado = new ConvidadoDTO();
         List<Convidado> convidados = convidadoRepository.findAll();
         Convidado convidado = getConvidadoSorteado(convidados);
-//        modelMapper.map(convidado, convidadoSorteado);
-        return convidadoSorteado;
+        modelMapper.map(convidado, convidadoSorteado);
+         return convidadoSorteado;
     }
 
     private Convidado getConvidadoSorteado(List<Convidado> convidados) {
